ebb9b6257b5044bdf56ea17413035049df6edaf0
raymond ong <raymondong.nus@gmail.com>
2022-08-16 10:16:17 +0800

    cleared if function

--- hello world/hello.go
+++ hello world/hello.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello ,"
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello world/hello_test.go
+++ hello world/hello_test.go
@@ -3,10 +3,30 @@ package main
 import "testing"
 
 func TestHello(t*testing.T) {
-	got := Hello("Chris")
-	want:= "Hello, Chris"
+	t.Run("saying hello to people", func(t*testing.T) {
+		got := Hello("Chris")
+		want:= "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 
-	if got != want {
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t*testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // this tells the test suite that this method is a helper
+	if got!= want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
